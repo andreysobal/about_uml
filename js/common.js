e333b689e51fc9edae893417126299309f7d7f81
andreysobal <say6287@gmail.com>
2017-02-15 06:34:02 +0300

    Fixed work of a to-top-button

--- js/common.js
+++ js/common.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
 	//to-top button
-	$("#top").click(function () {
-		$("body, html").animate({
-			scrollTop: 0
-		}, 800);
-		return false;
-	});
-
+	var top_show = 900;
+	var delay = 500;
+	
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > top_show) {
+			$('#top').fadeIn();
+		} else {
+			$('#top').fadeOut();
+		}//end if
+  });//end scroll
+	
+  $('#top').click(function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, delay);
+  });//end click
 
 });
